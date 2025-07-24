7f820e3af091128902e91d4f8ac32ff8e9edcbdb
Ramani Satishkumar <ramanisatishkumar2005@gmail.com>
2025-07-24 18:57:45 -0400

    Set up project

---
+++ src/test/java/com/leetcode/assistant/LeetcodeAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
